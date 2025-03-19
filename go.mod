3f1be3295a3bb867e9fe888edf71ef18c047f54b
KennyHuy <kennytran1989@gmail.com>
2025-03-19 11:25:27 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module demo
+
+go 1.23.4
+
+require github.com/kennytran1989/test-module v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kennytran1989/test-module"
+)
+
+func main() {
+	fmt.Println(test.Hello("kenny"))
+}
